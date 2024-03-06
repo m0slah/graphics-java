192d05b6e93a4837baf3265bcdb88c3bbc7bd670
m0slah <muhammedslahqadir@gmail.com>
2024-03-06 21:09:50 +0300

    use setResizebale,setLocation,setDefaultClose

--- src/myFrame1.java
+++ src/myFrame1.java
@@ -2,10 +2,13 @@ import javax.swing.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class myFrame1 extends JFrame{
-    public myFrame1(){
+class myFrame1 extends JFrame {
+    public myFrame1() {
         setTitle("My First Frame");
-                setSize(400,500);
+        setSize(400, 500);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(100,200);
     }
 
     public static void main(String[] a) {
