9cc2468cf7f1385d87c7375edd10dfa70067e044
m0slah <muhammedslahqadir@gmail.com>
2024-03-06 21:32:08 +0300

    make cneter div

--- src/myFrame1.java
+++ src/myFrame1.java
@@ -1,18 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class myFrame1 extends JFrame {
     public myFrame1() {
-        setTitle("My First Frame");
-        setSize(400, 500);
-        setResizable(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(100,200);
+        Toolkit tk=Toolkit.getDefaultToolkit();
+        Dimension d=tk.getScreenSize();
+        int sHeight=d.height;
+        int sWidth=d.width;
+        setSize(sWidth/2,sHeight/2);
+        setLocation(sWidth/4,sHeight/4);
     }
 
     public static void main(String[] a) {
         JFrame frame = new myFrame1();
-        frame.show();
+        frame.setVisible(true);
     }
 }
